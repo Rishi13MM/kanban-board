150651c57d7f62317dc500f85a2f673ef567573d
Rishi <rishimunijnp1@gmail.com>
2026-02-04 01:07:10 +0530

    Add drag and drop feature for tasks between columns

---
+++ script.js
@@ -0,0 +1,46 @@
+"use strict"
+
+const todoEl = document.getElementById("todo");
+const progressEl = document.getElementById("progress");
+const doneEl = document.getElementById("done");
+
+let dragElement = null;
+
+
+addDragEventsOnColumn(todoEl);
+addDragEventsOnColumn(doneEl);
+addDragEventsOnColumn(progressEl);
+
+
+const tasks = document.querySelectorAll(".task");
+
+tasks.forEach((task)=>{
+    task.addEventListener("drag", (e)=>{
+        dragElement = task;
+    });
+})
+
+
+function addDragEventsOnColumn(element) {
+    element.addEventListener("dragenter", (e)=>{
+        e.preventDefault();
+        element.classList.add("hover-over");
+    });
+
+    element.addEventListener("dragleave", (e)=>{
+        e.preventDefault();
+        element.classList.remove("hover-over");
+    });
+
+    element.addEventListener("dragover", (e)=>{
+        e.preventDefault();
+        console.log("hover..");
+    })
+
+    element.addEventListener("drop", (e)=> {
+        e.preventDefault();
+        element.append(dragElement);
+        element.classList.remove("hover-over");
+        console.log("Task dropped!");
+    });
+}
